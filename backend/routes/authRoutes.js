71fad09e7bd921d20b6cba6875a84ca5e8ac8cf1
PlatnumMan <fistonrwanda@gmail.com>
2023-09-17 22:34:20 +0200

    feat: implement access and refresh token rotation

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import loginUser from "../controllers/auth/loginController.js";
+import newAccessToken from "../controllers/auth/refreshTokenController.js";
 import registerUser from "../controllers/auth/registerController.js";
 import verifyUserEmail from "../controllers/auth/verifyEmailController.js";
 import { loginLimiter } from "../middleware/apiLimiter.js";
@@ -11,5 +12,7 @@ router.post("/register", registerUser);
 router.get("/verify/:emailToken/:userId", verifyUserEmail);
 
 router.post("/login", loginLimiter, loginUser);
+
+router.get("/new_access_token", newAccessToken);
 
 export default router;
